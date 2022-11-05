6875510a6219e9e38ee3ad0720427309a738ad40
victorfranca11 <victorhff11@gmail.com>
2022-11-05 14:45:58 +0000

    services

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 app = Flask(__name__)
 
+contador = 0
+
 @app.route("/")
 def hello_world():
     return "ola mundo"
@@ -10,6 +12,12 @@ def hello_world():
 @app.route("/health")
 def health():
     return Response("ok", status=200)
+
+@app.route("/counter")
+def counter():
+    global contador
+    contador += 1
+    return str(contador)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.environ["APP_PORT"], debug=True)
